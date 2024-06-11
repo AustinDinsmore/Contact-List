2175627b14ee3159bb7cdc2848fa1eb7ceedc7d2
Austin.Dinsmore <austdins@DESKTOP-C5NFOS3>
2024-06-10 19:44:14 -0600

    first tab done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ContactList from './components/ContactList';
+
+export default function App() {
+  
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+<div>
+
+</div>
