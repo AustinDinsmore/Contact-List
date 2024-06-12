d4ffd92d048d045e44e4ff0d618b89b49ec323f8
Austin.Dinsmore <austdins@DESKTOP-C5NFOS3>
2024-06-12 17:17:49 -0600

    fetching data and useEffect done

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -10,7 +10,18 @@ const dummyContacts = [
 export default function ContactList() {
 
   const [contacts, setContacts] = useState(dummyContacts);
-
+  useEffect(() =>{  async function fetchContacts() {
+    try {
+      const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+      const result = await response.json();
+      setContacts(result)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchContacts();
+}, []);
+console.log(contacts)
   return (
     <table>
       <thead>
